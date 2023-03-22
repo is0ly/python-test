6c1ec30009902c575b37877ed0efce285314560b
is0ly <iljaosipov@gmail.com>
2023-03-22 21:25:13 +0300

    ad actions

--- poetry_demo/__init__.py
+++ poetry_demo/__init__.py
@@ -1,15 +1,8 @@
-# from functools import reduce
+from functools import reduce
 
 
-# def sum(*args):
-#     return reduce(lambda x, y: x + y, args)
+def sum(*args):
+    return reduce(lambda x, y: x + y, args)
 
 
-# print(sum(3, 4, 5, 6, 65))
-
-
-def truncate(text, length):
-    return f"{text[:length]}..."
-
-
-print(truncate("some text", 4))
+print(sum(3, 4, 5, 6, 65))
